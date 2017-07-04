67de16f5c221fabe348123bff3abab2398786f73
Sean <sgilmore@michael-sean.com>
2017-07-03 23:00:38 -0400

    Bullshit homework

---
+++ public/script/test.js
@@ -0,0 +1,22 @@
+(;;){
+
+var i = 0
+
+for (var i = 0; i > -3; i--) {
+
+for (var i = 0; i < 50; i++) {
+  alert("Crazy yet?");
+}
+
+for (var i = 0; i < 5; i++) {
+  if (answers[i] === "yes") {
+    positive = 1;
+  } 
+}
+
+
+for (var i = 0; i <= 4; i++) {
+    if (cityToCheck === cleanestCities[i]) {
+        alert("It's one of the cleanest cities");
+    }
+}
